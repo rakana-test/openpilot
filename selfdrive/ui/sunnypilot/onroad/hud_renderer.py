6a2d4685d011a65b055b03ab916a93c274f90d41
Jason Wen <haibin.wen3@gmail.com>
2026-02-08 19:14:19 -0500

    [TIZI/TICI] ui: Green Light and Lead Departure elements (#1676)
    * init
    
    * big for now
    
    * only adjust for right dev ui for now
    
    * final
    
    * final final

--- selfdrive/ui/sunnypilot/onroad/hud_renderer.py
+++ selfdrive/ui/sunnypilot/onroad/hud_renderer.py
@@ -15,6 +15,7 @@ from openpilot.selfdrive.ui.sunnypilot.onroad.rocket_fuel import RocketFuel
 from openpilot.selfdrive.ui.sunnypilot.onroad.speed_limit import SpeedLimitRenderer
 from openpilot.selfdrive.ui.sunnypilot.onroad.smart_cruise_control import SmartCruiseControlRenderer
 from openpilot.selfdrive.ui.sunnypilot.onroad.turn_signal import TurnSignalController
+from openpilot.selfdrive.ui.sunnypilot.onroad.e2e_alerts import E2eAlertsRenderer
 
 
 class HudRendererSP(HudRenderer):
@@ -26,6 +27,7 @@ class HudRendererSP(HudRenderer):
     self.speed_limit_renderer = SpeedLimitRenderer()
     self.smart_cruise_control_renderer = SmartCruiseControlRenderer()
     self.turn_signal_controller = TurnSignalController()
+    self.e2e_alerts_renderer = E2eAlertsRenderer()
     self._torque_bar = TorqueBar(scale=3.0, always=True)
 
   def _update_state(self) -> None:
@@ -34,6 +36,7 @@ class HudRendererSP(HudRenderer):
     self.speed_limit_renderer.update()
     self.smart_cruise_control_renderer.update()
     self.turn_signal_controller.update()
+    self.e2e_alerts_renderer.update()
 
   def _render(self, rect: rl.Rectangle) -> None:
     super()._render(rect)
@@ -49,6 +52,7 @@ class HudRendererSP(HudRenderer):
     self.speed_limit_renderer.render(rect)
     self.smart_cruise_control_renderer.render(rect)
     self.turn_signal_controller.render(rect)
+    self.e2e_alerts_renderer.render(rect)
 
     if ui_state.rocket_fuel:
       self.rocket_fuel.render(rect, ui_state.sm)
